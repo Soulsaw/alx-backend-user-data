b83f8c36abccdb41078744b45f2ee6532ff556e1
Soulsaw <ssawadogo292@gmail.com>
2024-08-02 06:02:08 +0000

    fixing the annoted requirement

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Doc of the module"""
 import re
+from typing import List
 """Doc of the import"""
 
 
-def filter_datum(fields, redaction, message, separator):
-    """Doc of the funxtion"""
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     pattern = '|'.join(f'{field}=[^{separator}]*' for field in fields)
     return re.sub(pattern, lambda m: m.group().split('=')[0] +
                   '=' + redaction, message)
