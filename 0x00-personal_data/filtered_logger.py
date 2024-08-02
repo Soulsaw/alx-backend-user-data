47978bf07ed99f6babc3019bd8317748e2bfc519
Soulsaw <ssawadogo292@gmail.com>
2024-08-02 01:15:38 +0000

    fix the first task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
+"""Doc of the module"""
 import re
+"""Doc of the import"""
 
 
 def filter_datum(fields, redaction, message, separator):
-    pattern = '|'.join(f'{field}=[^;]*' for field in fields)
+    """Doc of the funxtion"""
+    pattern = '|'.join(f'{field}=[^{separator}]*' for field in fields)
     return re.sub(pattern, lambda m: m.group().split('=')[0] +
                   '=' + redaction, message)
