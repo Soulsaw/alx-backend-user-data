0e892f1d493032d5fb735c1b920a19321780a626
Soulsaw <ssawadogo292@gmail.com>
2024-08-04 22:52:27 +0000

    fix task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ def hash_password(password: str) -> bytes:
     """This function a hashed password"""
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """This function check if the password match"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
