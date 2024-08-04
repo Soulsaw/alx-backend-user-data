6aa3753763f7d4646f3ce89596eaaaabe94bdf97
Soulsaw <ssawadogo292@gmail.com>
2024-08-04 22:41:02 +0000

    fix all

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import bcrypt
+"""Import the modules"""
 
 
 def hash_password(password: str) -> str:
     """This function a hashed password"""
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed
