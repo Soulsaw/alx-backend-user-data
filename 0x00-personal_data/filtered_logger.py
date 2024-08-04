75b3d7c3f89a7dbb9c94b1845dee944b997abd78
Soulsaw <ssawadogo292@gmail.com>
2024-08-04 11:54:09 +0000

    fix task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Doc of the module"""
 import re
 from typing import List
+import logging
 """Doc of the import"""
 
 
@@ -11,3 +12,22 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = '|'.join(f'{field}=[^{separator}]*' for field in fields)
     return re.sub(pattern, lambda m: m.group().split('=')[0] +
                   '=' + redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """This method permit to filter values in incomming log record"""
+        record.message = filter_datum(self.fields, self.REDACTION,
+                                      record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
