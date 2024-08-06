3cda875239c3328faca16486206ac174494b478a
Soulsaw <ssawadogo292@gmail.com>
2024-08-06 16:29:17 +0000

    basic auth file

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,15 +6,20 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-from api.v1.auth.auth import Auth
 import os
 """All the require import"""
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
 AUTH_TYPE = os.getenv('AUTH_TYPE')
-auth = Auth()
+
+
+if AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Doc of the module"""
+from api.v1.auth.auth import Auth
+"""Import the require modules"""
+
+
+class BasicAuth(Auth):
+    """Doc of the BasicAuth inherit from Auth"""
