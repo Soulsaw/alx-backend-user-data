570f895b194cdf7d81d0e77740bc6e0070be477a
Soulsaw <ssawadogo292@gmail.com>
2024-08-06 17:05:30 +0000

    fix the base64 decode

--- 0x01-Basic_authentication/README.md
+++ 0x01-Basic_authentication/README.md
@@ -1,1 +1,2 @@
 # 0x01 Basic authentication
+## 8.Basic-Base64 decode
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Doc of the module"""
 from api.v1.auth.auth import Auth
+import base64
 """Import the require modules"""
 
 
@@ -16,3 +17,17 @@ class BasicAuth(Auth):
         if authorization_header[:6] != 'Basic ':
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """Return the decode of Base64 part of the Authorization"""
+        if not base64_authorization_header or\
+            type(base64_authorization_header) is not\
+                str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header).\
+                decode('utf-8')
+        except Exception:
+            return None
