588497ed83942db4bffb2e7cd9cd22860f3f3e58
Soulsaw <ssawadogo292@gmail.com>
2024-08-08 19:55:59 +0000

    add the session_auth file and update the README.md file

--- 0x02-Session_authentication/README.md
+++ 0x02-Session_authentication/README.md
@@ -1,4 +1,7 @@
 # 0x02. Session authentication
 ## Task 0
-Et moi et moi et moi!
+### Et moi et moi et moi!
 [Task 0]()
+## Task 1
+### Empty Session
+Create a SessionAuth that inherit from Auth and must stay empty
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Doc of the SessionAuth module"""
+from api.v1.auth.auth import Auth
+"""All the required import for the module"""
+
+
+class SessionAuth(Auth):
+    """Implementation of the session_auth"""
+    pass
