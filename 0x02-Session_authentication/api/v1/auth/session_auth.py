bd0c670d7c1fcb67d7afd63dcd8359f2f8e4fdc8
Soulsaw <ssawadogo292@gmail.com>
2024-08-08 20:49:45 +0000

    fix the create session uuid module use

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Doc of the SessionAuth module"""
 from api.v1.auth.auth import Auth
-from uuid import uuid4
+import uuid
 """All the required import for the module"""
 
 
@@ -13,8 +13,8 @@ class SessionAuth(Auth):
         """This function create an user session"""
         if user_id is None or type(user_id) is not str:
             return None
-        session_id  = uuid4()
-        self.user_id_by_session_id[str(session_id)] = user_id
+        session_id  = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
