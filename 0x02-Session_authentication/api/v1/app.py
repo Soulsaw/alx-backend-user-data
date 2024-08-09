0909baf33c514e2b0b018a99d0a43f3f17d2f879
Soulsaw <ssawadogo292@gmail.com>
2024-08-09 00:36:23 +0000

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,10 +54,10 @@ def before_request():
         return
     if auth.require_auth(request.path, url) is False:
         return
-    """ if auth.authorization_header(request) is None and\
+    if auth.authorization_header(request) is None and\
         auth.session_cookie(request) is\
             None:
-        abort(401) """
+        abort(401)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
