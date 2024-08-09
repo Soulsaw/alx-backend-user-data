b93e628d5f51d5bda9bf5a6dd3768ee2d48fa324
Soulsaw <ssawadogo292@gmail.com>
2024-08-09 00:03:21 +0000

    add the session_auth file

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Module of Users views
+"""
+from api.v1.views import app_views
+from flask import jsonify, request
+from models.user import User
+import os
+"""The require import"""
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_auth():
+    """This function return"""
+    email = request.form.get('email', None)
+    pwd = request.form.get('password', None)
+    if email is None or email == "":
+        return jsonify({"error": "email missing"}), 400
+    if pwd is None or pwd == "":
+        return jsonify({"error": "password missing"}), 400
+    user = User.search({"email": email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user[0].is_valid_password(pwd):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    cookie_token = auth.create_session(user[0].id)
+    output = jsonify(user[0].to_json())
+    session_name = os.getenv("SESSION_NAME")
+    output.set_cookie(session_name, cookie_token)
+    return output
