e88fb063f7e284421b42823091d18beffbed970c
Soulsaw <ssawadogo292@gmail.com>
2024-08-12 10:59:00 +0000

    fix db file

--- 0x03-user_authentication_service/README.md
+++ 0x03-user_authentication_service/README.md
@@ -1,1 +1,3 @@
 # 0x03. User authentucation service
+## 0. User model
+## 1. create user
---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from user import Base, User
+"""Requiered import"""
+
+
+class DB:
+    """DB class
+    """
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Implement the user store method"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs):
+        """Define the find_user_by method"""
+        try:
+            query = self._session.query(User)
+            for key, value in kwargs.items():
+                column = getattr(User, key, None)
+                if column is None:
+                    raise InvalidRequestError
+                query = query.filter(column == value)
+            result = query.one()
+            return result
+        except NoResultFound:
+            raise NoResultFound
+        except InvalidRequestError:
+            raise InvalidRequestError
