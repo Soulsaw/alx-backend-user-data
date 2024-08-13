e35a4fcf480239c104f307babe620080a1d6fdf1
Soulsaw <ssawadogo292@gmail.com>
2024-08-13 16:21:34 +0000

    fix the cookie

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Doc of the app module"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 """Required import"""
 app = Flask(__name__)
@@ -24,6 +24,21 @@ def users():
             return jsonify({"email": user.email, "message": "user created"})
         except ValueError:
             return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Handle the login function"""
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            resp = jsonify({"email": email, "message": "logged in"})
+            resp.set_cookie("session_id", session_id)
+            return resp
+        else:
+            abort(401)
 
 
 if __name__ == "__main__":
