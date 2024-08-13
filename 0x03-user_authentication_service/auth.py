6031c0788287a577b32f61452840554783020a70
Soulsaw <ssawadogo292@gmail.com>
2024-08-13 15:15:21 +0000

    fix the token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,6 +52,23 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        """Return the session id with a giving email
+
+        Args:
+            email (str): The user email
+
+        Returns:
+                session_id (str): Return the user session id
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_token = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_token)
+            return session_token
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """Take a password as argument in return a bytes
