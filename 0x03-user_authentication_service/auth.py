46985370d64a43a9db5355e032ea105341461534
Soulsaw <ssawadogo292@gmail.com>
2024-08-13 11:41:10 +0000

    fix the register task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python3
 """Doc of the auth module"""
 import bcrypt
-"""Require import"""
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+"""Required import"""
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user in the database
+
+        Args:
+            email (str): The user email
+            password (str): The user giving password
+
+        Returns:
+                User: Return the user object instance
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists.")
+        except NoResultFound:
+            hashed = _hash_password(password)
+            user = self._db.add_user(email=email, hashed_password=hashed)
+            return user
 
 
 def _hash_password(password: str) -> bytes:
