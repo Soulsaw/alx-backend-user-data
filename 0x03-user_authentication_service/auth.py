84a549a14e62016267b5445bbb46d504728f142a
Soulsaw <ssawadogo292@gmail.com>
2024-08-13 14:11:22 +0000

    fix the valid login from the auth file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,6 +32,25 @@ class Auth:
             user = self._db.add_user(email=email, hashed_password=hashed)
             return user
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """Valid the user login
+
+        Args:
+            email (str): The user email
+            password (str): The user password
+
+        Returns:
+            bool: True or False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode(), user.hashed_password):
+                return True
+            else:
+                return False
+        except NoResultFound:
+            return False
+
 
 def _hash_password(password: str) -> bytes:
     """Take a password as argument in return a bytes
